c4cc4c4b3030e6bcfcd13609a40899eb65d33ecf
+collins <+collinskipngetich60@gmail.com>
2024-01-21 19:37:24 +0300

    completed

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Write your algorithm herefunction isPalindrome(word) {
+  const cleanedStr = removeNonAlphabetic(toLowercase(word));
+  return cleanedStr === cleanedStr.split('').reverse().join('');
 }
+
 
 /* 
   Add your pseudocode here
 */
+function toLowercase(str) {
+    return str.toLowerCase();
+}
+function removeNonAlphabetic(str) {
+    return str.replace(/[^a-z]/g, '');
+}
 
 /*
   Add written explanation of your solution here
 */
-
+module.exports = isPalindrome;
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
@@ -21,5 +30,3 @@ if (require.main === module) {
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
-
-module.exports = isPalindrome;
